64ef2028c7d3368a77f65e31e53fcbd78a78734a
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-10-07 11:16:21 +0300

    Droid lab 1

---
+++ droidlab1/app/src/main/java/com/example/droidlab1/Droidlab1.java
@@ -0,0 +1,28 @@
+package com.example.droidlab1;
+
+public class Droidlab1 {
+    int age;
+    String name;
+    String regNo;
+    String phone_number;
+    String email;
+
+    //constructor - any method which has the same name as the class name
+    // public Droidlab1 (String name, int age, String regNo, String phone_number, String email) {
+     public Droidlab1 () {
+
+
+        this.age = age;
+        this.name = name;
+        this.regNo = regNo;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+
+    //methods must be within a class
+
+    public void display () {
+
+    }
+
+}
